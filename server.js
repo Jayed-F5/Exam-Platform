85211d0d25594e651353e2301f5697ac56af8981
Jayed-F5 <mohamedelamine.jayed@etu.uae.ac.ma>
2025-04-23 00:00:53 +0100

    use dynamic port

--- server.js
+++ server.js
@@ -2,20 +2,20 @@ const express = require('express');
 const session = require('express-session');
 const path = require('path');
 const bodyParser = require('body-parser');
-
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
+const isProduction = process.env.NODE_ENV === 'production';
+
 
 // Middleware
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
-
 app.use(session({
   secret: 'your_secret_key_here',
   resave: false,
   saveUninitialized: false,
-  cookie: { secure: false, maxAge: 1000 * 60 * 60 * 24 } // 1 day
+  cookie: { secure: isProduction, maxAge: 1000 * 60 * 60 * 24 } // 1 day
 }));
 
 // Routes
