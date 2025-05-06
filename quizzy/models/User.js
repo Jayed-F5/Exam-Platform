1aed3cd7540ef14b4c33e2f102dccac98486c026
Jayed-F5 <mohamedelamine.jayed@etu.uae.ac.ma>
2025-05-07 00:55:09 +0100

    version final

--- quizzy/models/User.js
+++ quizzy/models/User.js
@@ -1,27 +1,31 @@
+
 const db = require('./db');
 
+// Function to retrieve a user by their email
 const getUserByEmail = async (email) => {
-  const [rows] = await db.promise().query('SELECT * FROM users WHERE email = ?', [email]);
-  return rows[0];
+    const [rows] = await db.promise().query('SELECT * FROM users WHERE email = ?', [email]);
+    return rows[0];
 };
 
+// Function to create a new user in the database
 const createUser = async (userData) => {
-  const {
-    email, password, firstname, lastname,
-    birthdate, gender, institution, field
-  } = userData;
-a
-  const insertQuery = `
-    INSERT INTO users (email, password, firstname, lastname, birthdate, gender, institution, field)
-    VALUES (?, ?, ?, ?, ?, ?, ?, ?)
-  `;
+    const {
+        email, password, firstname, lastname,
+        birthdate, gender, institution, field
+    } = userData;
 
-  await db.promise().query(insertQuery, [
-    email, password, firstname, lastname, birthdate, gender, institution, field
-  ]);
+    const insertQuery = `
+        INSERT INTO users (email, password, firstname, lastname, birthdate, gender, institution, field)
+        VALUES (?, ?, ?, ?, ?, ?, ?, ?)
+    `;
+
+    await db.promise().query(insertQuery, [
+        email, password, firstname, lastname, birthdate, gender, institution, field
+    ]);
 };
 
+// Exporting the functions for use in other parts of the application
 module.exports = {
-  getUserByEmail,
-  createUser
+    getUserByEmail,
+    createUser
 };
